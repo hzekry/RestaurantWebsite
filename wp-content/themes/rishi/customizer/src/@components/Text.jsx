b2b966ce5333f7c2d862db5c2b5e834860eaa143
hzekry <96088926+hzekry@users.noreply.github.com>
2024-03-02 02:49:14 +0200

    publish

---
+++ wp-content/themes/rishi/customizer/src/@components/Text.jsx
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled"
+
+const InputField = styled.div`
+	&[data-type="social"], &[data-type="link"]{
+		position: relative;
+		&::before{
+			content: "https://";
+			color: #42474B;
+			position: absolute;
+			left: 0;
+			top: 0;
+			height: 100%;
+			padding: 10.5px 10px;
+			background-color: var(--cw__background-color);
+			border-radius: var(--cw__border-radius) 0 0 var(--cw__border-radius);
+			display: flex;
+    		align-items: center;
+			border: 1px solid var(--cw__border-color);
+			border-right: 0;
+			opacity: .5;
+		}
+		input{
+			padding-left: 75px;
+		}
+	}
+`
+
+const Text = ({ onChange, type, dataType, ...props }) => {
+	return <InputField className="cw__input-field" data-type={dataType}>
+		<input type={type || 'text'} onChange={(e) => onChange(e.target.value)} {...props} />
+	</InputField>
+}
+
+export default Text
---
+++ wp-content/themes/rishi/inc/dashboard/pages/index.js
@@ -0,0 +1,25 @@
+import { useCurrentTab } from "../components/Hooks";
+import { applyFilters } from '@wordpress/hooks';
+import General from "./general";
+import StarterSettings from "./startersettings";
+
+const PageComponent = () => {
+	const currentPage = useCurrentTab();
+
+	if ("addons" === currentPage) {
+		return applyFilters('rishi_dashboard_pages', null);
+	} else if ("extensions" === currentPage || "usefulplugin" === currentPage || 'license' === currentPage ) {
+		return applyFilters('rishi_dashboard_components', currentPage);
+	} else {
+		switch (currentPage) {
+			case "general":
+				return <General />
+			case "starter_sites":
+				return <StarterSettings />
+			default:
+				return <General />
+		}
+	}
+}
+
+export default PageComponent;
